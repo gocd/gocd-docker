ab91fef93e1006ea6ca97af312fde55408a4c89a
Aravind SV <arvind.sv@gmail.com>
2015-02-12 16:36:47 -0500

    Use default auto register key on agent as well.

--- gocd-agent/go-agent-start.sh
+++ gocd-agent/go-agent-start.sh
@@ -10,7 +10,9 @@ sed -i -e 's/GO_SERVER=.*/GO_SERVER='$GO_SERVER'/' /etc/default/go-agent
 
 mkdir -p /var/lib/go-agent/config
 /bin/rm -f /var/lib/go-agent/config/autoregister.properties
-if [ -n "$AGENT_KEY" ]; then echo "agent.auto.register.key=$AGENT_KEY" >>/var/lib/go-agent/config/autoregister.properties; fi
+
+AGENT_KEY="${AGENT_KEY:-123456789abcdef}"
+echo "agent.auto.register.key=$AGENT_KEY" >/var/lib/go-agent/config/autoregister.properties
 if [ -n "$AGENT_RESOURCES" ]; then echo "agent.auto.register.resources=$AGENT_RESOURCES" >>/var/lib/go-agent/config/autoregister.properties; fi
 
 /sbin/setuser go /etc/init.d/go-agent start
