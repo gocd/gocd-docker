38601ec6d21e2dd0b6132a8e4cb1c779605f7bfc
Aravind SV <arvind.sv@gmail.com>
2016-03-10 18:20:13 -0500

    Configure using env vars directly
    * No need to mess around with /etc/default/go-agent
    * Also, run agent as root, instead of go user

--- gocd-agent/go-agent-start.sh
+++ gocd-agent/go-agent-start.sh
@@ -1,28 +1,25 @@
 #!/bin/bash
 
-GO_SERVER=${GO_SERVER:-go-server}
 AGENT_KEY=${AGENT_KEY:-123456789abcdef}
-DAEMON=${DAEMON:-N}
 
-COLOR_START="[01;34m"
-COLOR_END="[00m"
+export GO_SERVER=${GO_SERVER:-go-server}
+export GO_SERVER_PORT=8153
+export DAEMON=${DAEMON:-N}
+export AGENT_WORK_DIR=/var/lib/go-agent
+export JAVA_HOME=/usr/lib/jvm/java-7-openjdk-amd64/jre
 
 mkdir -p /var/lib/go-agent/config /var/run/go-agent /var/log/go-agent
 chown -R go:go /var/lib/go-agent/config /var/run/go-agent /var/log/go-agent
 
+rm -f /etc/default/go-agent
+
 autoregister_file=/var/lib/go-agent/config/autoregister.properties
-logfiles=(go-agent-bootstrapper.out.log go-agent-launcher.log go-agent-stderr.log go-agent-stdout.log go-agent.log)
-
-echo -e "${COLOR_START}Starting Go Agent to connect to server $GO_SERVER ...${COLOR_END}"
-sed -i \
-    -e 's/GO_SERVER=.*/GO_SERVER='$GO_SERVER'/' \
-    -e 's/DAEMON=.*/DAEMON='$DAEMON'/' \
-    /etc/default/go-agent
-
 echo "agent.auto.register.key=$AGENT_KEY" >$autoregister_file
 if [ -n "$AGENT_RESOURCES" ]; then echo "agent.auto.register.resources=$AGENT_RESOURCES" >>$autoregister_file; fi
 if [ -n "$AGENT_ENVIRONMENTS" ]; then echo "agent.auto.register.environments=$AGENT_ENVIRONMENTS" >>$autoregister_file; fi
 chown go:go $autoregister_file
 
+logfiles=(go-agent-bootstrapper.out.log go-agent-launcher.log go-agent-stderr.log go-agent-stdout.log go-agent.log)
 (cd /var/log/go-agent; touch "${logfiles[@]}"; chown go:go "${logfiles[@]}"; tail -F -v "${logfiles[@]}" & disown)
-exec sudo -u go -i /usr/share/go-agent/agent.sh
+
+exec /usr/share/go-agent/agent.sh
