be1dc0186f4b1e1b1bc80cfdcfb824448c49647a
Aravind SV <arvind.sv@gmail.com>
2016-03-10 16:50:40 -0500

    Move away from phusion/baseimage
    * phusion/baseimage is not being updated quick enough, to take care of security vulnerabilities for instance.
    
    Also in this commit:
    
    * Use dumb_init instead to provide a proper init process, to propagate signals and take over zombies.
    * Use sudo to lauch the agent process, since it propagates signals properly, unlike su and bash.
    * tail all logs of the agent
    * Use openjdk-7-jre baseimage directly. Still not ideal (it uses an old debian baseimage), but better than before.

--- gocd-agent/go-agent-start.sh
+++ gocd-agent/go-agent-start.sh
@@ -1,19 +1,28 @@
 #!/bin/bash
 
 GO_SERVER=${GO_SERVER:-go-server}
+AGENT_KEY=${AGENT_KEY:-123456789abcdef}
+DAEMON=${DAEMON:-N}
 
 COLOR_START="[01;34m"
 COLOR_END="[00m"
 
+mkdir -p /var/lib/go-agent/config /var/run/go-agent /var/log/go-agent
+chown -R go:go /var/lib/go-agent/config /var/run/go-agent /var/log/go-agent
+
+autoregister_file=/var/lib/go-agent/config/autoregister.properties
+logfiles=(go-agent-bootstrapper.out.log go-agent-launcher.log go-agent-stderr.log go-agent-stdout.log go-agent.log)
+
 echo -e "${COLOR_START}Starting Go Agent to connect to server $GO_SERVER ...${COLOR_END}"
-sed -i -e 's/GO_SERVER=.*/GO_SERVER='$GO_SERVER'/' /etc/default/go-agent
+sed -i \
+    -e 's/GO_SERVER=.*/GO_SERVER='$GO_SERVER'/' \
+    -e 's/DAEMON=.*/DAEMON='$DAEMON'/' \
+    /etc/default/go-agent
 
-mkdir -p /var/lib/go-agent/config
-/bin/rm -f /var/lib/go-agent/config/autoregister.properties
+echo "agent.auto.register.key=$AGENT_KEY" >$autoregister_file
+if [ -n "$AGENT_RESOURCES" ]; then echo "agent.auto.register.resources=$AGENT_RESOURCES" >>$autoregister_file; fi
+if [ -n "$AGENT_ENVIRONMENTS" ]; then echo "agent.auto.register.environments=$AGENT_ENVIRONMENTS" >>$autoregister_file; fi
+chown go:go $autoregister_file
 
-AGENT_KEY="${AGENT_KEY:-123456789abcdef}"
-echo "agent.auto.register.key=$AGENT_KEY" >/var/lib/go-agent/config/autoregister.properties
-if [ -n "$AGENT_RESOURCES" ]; then echo "agent.auto.register.resources=$AGENT_RESOURCES" >>/var/lib/go-agent/config/autoregister.properties; fi
-if [ -n "$AGENT_ENVIRONMENTS" ]; then echo "agent.auto.register.environments=$AGENT_ENVIRONMENTS" >>/var/lib/go-agent/config/autoregister.properties; fi
-
-/sbin/setuser go /etc/init.d/go-agent start
+(cd /var/log/go-agent; touch "${logfiles[@]}"; chown go:go "${logfiles[@]}"; tail -F -v "${logfiles[@]}" & disown)
+exec sudo -u go -i /usr/share/go-agent/agent.sh
